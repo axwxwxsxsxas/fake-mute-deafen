6da7ff29a729ff9b8e8462dc483a34bbbfb1a553
axwxwxsxsxas <sofiaadali445@gmail.com>
2026-09-20 14:44:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import { storage } from "@vendetta/plugin";
 import { findByProps, findByStoreName } from "@vendetta/metro";
 import { instead, after } from "@vendetta/patcher";
-import { ReactNative } from "@vendetta/metro/common";
+import { React } from "@vendetta/metro/common";
 import { Forms } from "@vendetta/ui/components";
 
 const { FormSwitchRow, FormSection } = Forms;
 
 storage.enabled ??= false;
-
 let patches = [];
 
 export default {
@@ -15,70 +14,57 @@ export default {
     const voiceModule = findByProps("setSelfMute", "setSelfDeaf");
 
     if (voiceModule) {
-      patches.push(
-        instead("setSelfMute", voiceModule, (args, orig) => {
-          if (storage.enabled) return orig(true);
-          return orig(...args);
-        })
-      );
+      patches.push(instead("setSelfMute", voiceModule, (args, orig) => {
+        if (storage.enabled) return orig(true);
+        return orig(...args);
+      }));
 
-      patches.push(
-        instead("setSelfDeaf", voiceModule, (args, orig) => {
-          if (storage.enabled) return orig(true);
-          return orig(...args);
-        })
-      );
+      patches.push(instead("setSelfDeaf", voiceModule, (args, orig) => {
+        if (storage.enabled) return orig(true);
+        return orig(...args);
+      }));
     }
 
     const MediaEngineStore = findByStoreName("MediaEngineStore");
     if (MediaEngineStore) {
-      patches.push(
-        after("getMediaEngine", MediaEngineStore, (_, ret) => {
-          if (storage.enabled && ret) {
-            try {
-              if (ret.setOutputMuted) ret.setOutputMuted(false);
-              if (ret.setInputMuted) ret.setInputMuted(false);
-            } catch (e) {}
-          }
-          return ret;
-        })
-      );
+      patches.push(after("getMediaEngine", MediaEngineStore, (_, ret) => {
+        if (storage.enabled && ret) {
+          try {
+            ret.setOutputMuted?.(false);
+            ret.setInputMuted?.(false);
+          } catch {}
+        }
+        return ret;
+      }));
     }
   },
 
   onUnload() {
-    patches.forEach((p) => p());
+    patches.forEach(p => p());
     patches = [];
   },
 
   settings: () => {
-    const [enabled, setEnabled] = ReactNative.React.useState(storage.enabled);
+    const [enabled, setEnabled] = React.useState(storage.enabled);
 
     return (
-      ReactNative.React.createElement(
-        FormSection,
-        { title: "Fake Mute & Deafen" },
-        ReactNative.React.createElement(FormSwitchRow, {
-          label: "Activar Fake Mute + Deafen",
-          subLabel: "Aparecerás muteado y ensordecido, pero podrás seguir escuchando",
-          value: enabled,
-          onValueChange: (v) => {
+      <FormSection title="Fake Mute & Deafen">
+        <FormSwitchRow
+          label="Activar Fake Mute + Deafen"
+          subLabel="Aparecerás muteado y ensordecido, pero podrás seguir escuchando"
+          value={enabled}
+          onValueChange={(v) => {
             storage.enabled = v;
             setEnabled(v);
 
             const voiceModule = findByProps("setSelfMute", "setSelfDeaf");
             if (voiceModule) {
-              if (v) {
-                voiceModule.setSelfMute(true);
-                voiceModule.setSelfDeaf(true);
-              } else {
-                voiceModule.setSelfMute(false);
-                voiceModule.setSelfDeaf(false);
-              }
+              voiceModule.setSelfMute(v);
+              voiceModule.setSelfDeaf(v);
             }
-          },
-        })
-      )
+          }}
+        />
+      </FormSection>
     );
-  },
+  }
 };
